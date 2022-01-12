9fa82d1e92bc644b01c7cdcde4b34cb25bcf1772
zaphod424 <zaphod467@gmail.com>
2022-01-12 21:22:45 +0000

    bars work!

--- Sort-Visualiser/Sort-Visualiser.cpp
+++ Sort-Visualiser/Sort-Visualiser.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
+#include <random>
+#include <ctime>
 
 class Button {
 public:
@@ -19,8 +21,8 @@ public:
         name = a;
         xpos = b;
         sf::RectangleShape icon1(buttonSize);
-        sf::Color colour = sf::Color::White;
-        sf::Color textColour = sf::Color::Black;
+        colour = sf::Color::White;
+        textColour = sf::Color::Black;
 
         icon = icon1;
         icon.setFillColor(colour);
@@ -31,23 +33,54 @@ public:
     }
 };
 
+class Bar {
+public:
+    int value, xpos, ypos;
+    sf::Color colour;
+    sf::RectangleShape icon;
+
+    Bar(int v, sf::Vector2f barSize, int x, int y) {
+
+
+        
+        value = v;
+        xpos = x;
+        ypos = y;
+        barSize.y = barSize.y * value;
+        sf::RectangleShape icon1(barSize);
+        colour = sf::Color::White;
+        icon = icon1;
+        icon.setFillColor(colour);
+        icon.setPosition(xpos, ypos-barSize.y);
+    };
+
+    void reposition(int x) {
+
+        xpos = x;
+        icon.setPosition(xpos, ypos);
+    };
+
+};
+
 int main()
 {
 
     const int n_buttons = 7;
     float scale = 0.6;
+    int max_val = 10;
 
     //setting window parameters
     int windowWidth = sf::VideoMode::getDesktopMode().width * scale;
     int windowHeight = sf::VideoMode::getDesktopMode().height * scale;
 
-
+    std::srand(std::time(nullptr));
     
     // loading font
     sf::Font font;
     if (!font.loadFromFile("arial.ttf")) 
     {
         std::cout << "Error no font";
+        return -1;
     }
 
     //setting banner parameters
@@ -66,6 +99,27 @@ int main()
         Button button(buttonNameList[i], buttonSize.x * i , buttonSize);
         buttonList.push_back(button);
     }
+
+
+    //setting bar parameters
+
+    int n_bars = 6;
+    
+
+    int ypos = windowHeight * 0.9;
+    sf::Vector2f barSize(((windowWidth * 0.9) /n_bars) - 4 , (ypos - (uiSize.y * 2)) /max_val );
+
+    std::vector<Bar> barList;
+    
+
+    //creating bar objects
+    for (int i = 0; i < n_bars; i++) {
+            
+        int value = rand() % max_val;
+        Bar newBar(value, barSize, ((windowWidth * 0.05)) + (barSize.x + 4)*i, ypos);
+        barList.push_back(newBar);
+    }
+
 
     //window loop
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Sorting Algorithm Visualiser");
@@ -96,6 +150,16 @@ int main()
             window.draw(text);
 
         }
+
+        //drawing bars
+
+        for (int i = 0; i < n_bars; i++) {
+
+            window.draw(barList[i].icon);
+        }
+
+ 
+
 
 
         window.display();       //end of clear_draw_display
